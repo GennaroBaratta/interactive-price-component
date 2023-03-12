ac9a364e9ebb6133075a314ae145573b75e6f634
Morksvart <arkantos405@gmail.com>
2023-03-12 01:58:49 +0100

    copy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
+import { Manrope } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+const manrope = Manrope({ subsets: ['latin'], weight: ['600', '800'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={manrope.className} {...pageProps} />
 }
